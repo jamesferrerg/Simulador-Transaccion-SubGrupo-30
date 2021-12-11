41227b8a24b9dbb31d73f5398d475b178f8a86b9
jamesferrerg <jamesjfgv@gmail.com>
2021-12-11 12:28:06 -0500

    Ultima entrega

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -34,11 +34,18 @@ public class Conexion {
 			Class.forName(driver);
 			con = DriverManager.getConnection(url, user, pwd);
 			JOptionPane.showMessageDialog(null, "Conexión establecida con éxito");
-			int transaccion = Integer.parseInt(JOptionPane.showInputDialog(
-					"Seleccione la transacción que desee de acuerdo al número: \n1. Crear cliente \n" + "2. Salir \n"));
+			int transaccion = Integer.parseInt(JOptionPane
+					.showInputDialog("Seleccione la transacción que desee de acuerdo al número: \n1. Crear cliente \n"
+							+ "2. Retirar \n" + "3. Consulta de datos \n" + "4. Salir \n"));
 			switch (transaccion) {
 			case 1:
 				crearCliente(con, pstm);
+				break;
+			case 2:
+				retiro(con, pstm);
+				break;
+			case 3:
+				consultaDatos(con, pstm);
 				break;
 			default:
 				System.out.println("Salio sin realizar algun tipo de transacción");
@@ -88,6 +95,93 @@ public class Conexion {
 		} else {
 			con.rollback();
 			throw new RuntimeException("Error insertando la fila");
+		}
+	}
+	
+	public void retiro(Connection con, PreparedStatement pstm) throws SQLException, ParseException {
+		ResultSet rs = null;
+		JOptionPane.showMessageDialog(null, "Iniciando retiro de la cuenta");
+		// Preparar query para consultar el saldo actual del cliente
+		String sqlSaldoInicial = "SELECT cu.saldo FROM banco_db.cuenta cu WHERE cu.id_cuenta=1";
+		pstm = con.prepareStatement(sqlSaldoInicial);
+		rs = pstm.executeQuery();
+		// Mostrar por visor el saldo del cliente
+		if (rs.next()) {
+			JOptionPane.showMessageDialog(null, "Su saldo actual es: " + rs.getString(1));
+		}
+		
+		// Insertar el valor del retiro a la tabla moviento como registro del retiro
+		String sql1 = "INSERT INTO `banco_db`.`movimiento` (`id_movimiento`, `valor`, `fecha`, `tipo_id`, `cuenta_id`)";
+		sql1 += "VALUES (?, ?, ?, ?, ?)";
+		
+		int id_movimiento = Integer.parseInt(JOptionPane.showInputDialog("id_movimiento: "));
+		Double valor = Double.parseDouble(JOptionPane.showInputDialog("Valor a retirar: "));
+		String fecha = JOptionPane.showInputDialog("fecha: YYYY/MM/DD ");
+		
+		pstm = con.prepareStatement(sql1);
+		pstm.setInt(1, id_movimiento);
+		pstm.setDouble(2, valor);
+		pstm.setString(3, fecha);
+		pstm.setInt(4, 2);
+		pstm.setInt(5, 1);
+		
+		int aplique = pstm.executeUpdate();
+		if (aplique == 1) {
+			System.out.println(aplique + " fila insertada con éxito");
+			// Nota : esto confirmará implícitamente la transacción activa y creará una nueva
+			con.setAutoCommit(false);
+			con.commit();
+		} else {
+			con.rollback();
+			throw new RuntimeException("Error insertando la fila");
+		}
+		
+		// Actualizar el valor del saldo restando el valor actual del saldo menos el valor del retiro
+		String sql2 = "UPDATE banco_db.cuenta cu\n"
+				+ "SET cu.saldo=((SELECT cu.saldo)-(SELECT M.valor FROM banco_db.movimiento M ORDER BY M.id_movimiento DESC LIMIT 1))\n"
+				+ "WHERE cu.id_cuenta=?";
+		
+		pstm = con.prepareStatement(sql2);
+		pstm.setInt(1,1);
+		
+		int aplique2 = pstm.executeUpdate();
+		if (aplique2 == 1) {
+			System.out.println(aplique + " campo actualizado con éxito");
+			// Nota : esto confirmará implícitamente la transacción activa y creará una nueva
+			con.setAutoCommit(false);
+			con.commit();
+		} else {
+			con.rollback();
+			throw new RuntimeException("Error insertando la fila");
+		}
+		
+		JOptionPane.showMessageDialog(null, "Iniciando retiro de la cuenta");
+		String sqlSaldoFinal = "SELECT cu.saldo FROM banco_db.cuenta cu WHERE cu.id_cuenta=1";
+		pstm = con.prepareStatement(sqlSaldoFinal);
+		rs = pstm.executeQuery();
+		if (rs.next()) {
+			JOptionPane.showMessageDialog(null, "Su saldo final es: " + rs.getString(1));
+		}
+		
+	}
+	
+	public void consultaDatos(Connection con, PreparedStatement pstm) throws SQLException, ParseException{
+		ResultSet rs = null;
+		// Realizacion de consulta sobre ultimo movimiento, saldo y cliente
+		String sql = "SELECT M.valor, M.fecha, cu.saldo, cl.nombres, cl.apellidos, cl.numero_identificacion FROM banco_db.cuenta AS cu \n"
+				+ "INNER JOIN banco_db.movimiento AS M\n" + "ON cu.id_cuenta=M.cuenta_id\n"
+				+ "INNER JOIN banco_db.cliente AS cl\n" + "ON cl.id_cliente=cu.cliente_id\n" + "WHERE cl.id_cliente=4\n"
+				+ "ORDER BY M.id_movimiento DESC LIMIT 1";
+		
+		pstm = con.prepareStatement(sql);
+		rs = pstm.executeQuery();
+		// Mostrar en la ventana la consulta realizada
+		if (rs.next()) {
+			JOptionPane.showMessageDialog(null,
+					"Consulta datos cliente \n" + "Nombre: " + rs.getString(4) + " " + rs.getString(5) + "\n"
+							+ "Numero de identificacion: " + rs.getString(6) + "\n" + "Valor ultimo movimiento: "
+							+ rs.getString(1) + "\n" + "Fecha del movimiento: " + rs.getString(2) + "\n"
+							+ "Saldo actual: " + rs.getString(3) + "\n");
 		}
 	}
 
